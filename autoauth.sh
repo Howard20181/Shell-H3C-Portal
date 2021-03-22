39fa72165532ff8ba36de0575ea084f36677f057
Howard20181 <40033067+Howard20181@users.noreply.github.com>
2021-03-22 09:47:51 +0800

    Fix early auth loop

--- autoauth.sh
+++ autoauth.sh
@@ -64,17 +64,18 @@ function get_json_value() {
 }
 
 function check_SHOULD_STOP() {
-    TIME_STOP1=$(date -d "$(date '+%Y-%m-%d 23:15:00')" +%s)
-    TIME_STOP2=$(date -d "$(date '+%Y-%m-%d 06:00:00')" +%s)
+    SHOULD_STOP=false
+    SLEEP_TIME="1"
+    local TIME_STOP1=$(date -d "$(date '+%Y-%m-%d 23:15:00')" +%s)
+    local TIME_STOP2=$(date -d "$(date '+%Y-%m-%d 07:00:00')" +%s)
     local TIME_CUR=$(date +%s)
-    if [ "${portServIncludeFailedCode}" != "63027" ]; then
-        SHOULD_STOP=false
-        SLEEP_TIME="1"
-    elif [ ${TIME_CUR} -gt ${TIME_STOP1} ] || [ ${TIME_CUR} -lt ${TIME_STOP2} ]; then
-        SHOULD_STOP=true
-        logger -t "${BaseName}" -p user.info "EXIT!"
-        echo "EXIT!"
-        exit
+    if [ "${portServIncludeFailedCode}" = "63027" ]; then
+        if [ ${TIME_CUR} -gt ${TIME_STOP1} ] || [ ${TIME_CUR} -lt ${TIME_STOP2} ]; then
+            SHOULD_STOP=true
+            logger -t "${BaseName}" -p user.info "EXIT!"
+            echo "EXIT!"
+            exit
+        fi
     fi
 }
 
@@ -90,11 +91,11 @@ function doHeartBeat() {
     logger -t "${BaseName}" -p user.info "Start do HeartBeat"
     local TIME=$(date '+%Y-%m-%d %H:%M:%S')
     echo "Start do HeartBeat ${TIME}"
-    userDevPort=$(get_json_value $v_json userDevPort)
-    userDevPort_ENCODEURL=$(encodeURIComponent $userDevPort)
-    userStatus=$(get_json_value $v_json userStatus)
-    serialNo=$(get_json_value $v_json serialNo)
-    v_Language=$(get_json_value $v_json clientLanguage)
+    local userDevPort=$(get_json_value $v_json userDevPort)
+    local userDevPort_ENCODEURL=$(encodeURIComponent $userDevPort)
+    local userStatus=$(get_json_value $v_json userStatus)
+    local serialNo=$(get_json_value $v_json serialNo)
+    local v_Language=$(get_json_value $v_json clientLanguage)
 
     doHeartBeat_INFO=$(curl -s ''$appRootUrl'page/doHeartBeat.jsp' \
         -H 'Origin: http://'$byodserverip':'$byodserverhttpport'' \
@@ -115,11 +116,11 @@ function start_auth() {
     echo "Start auth"
     logger -t "${BaseName}" -p user.info "Send Login request"
     echo "Send Login request"
-    PWD_BASE64="$(printf $PWD | base64)"
-    PWD_BASE64_ENCODEURL=$(encodeURIComponent $PWD_BASE64)
+    local PWD_BASE64="$(printf $PWD | base64)"
+    local PWD_BASE64_ENCODEURL=$(encodeURIComponent $PWD_BASE64)
     appRootUrl="http://$byodserverip:$byodserverhttpport/portal/"
-    appRootUrl_ENCODEURL=$(encodeURIComponent $appRootUrl)
-    DATA=$(curl -s ''$appRootUrl'pws?t=li' \
+    local appRootUrl_ENCODEURL=$(encodeURIComponent $appRootUrl)
+    local DATA=$(curl -s ''$appRootUrl'pws?t=li' \
         -H 'Accept: text/plain, */*;' \
         -H 'User-Agent: Mozilla/5.0 (Linux; Android; wv)' \
         -H 'Content-Type: application/x-www-form-urlencoded;' \
@@ -132,18 +133,19 @@ function start_auth() {
     if [ ! -n "${DATA}" ]; then #未收到回应，网络错误
         logger -t "${BaseName}" -p user.err "Network error"
         echo "Network error"
+        start_auth
     else #收到回应，可以连接上认证服务器
         logger -t "${BaseName}" -p user.info "Analyzing authentication results"
         echo "Analyzing authentication results"
-        DATA_ENCODEURL=$(printf "%s" "${DATA}==" | base64 -d)
-        JSON=$(urldecode "${DATA_ENCODEURL}")
+        local DATA_ENCODEURL=$(printf "%s" "${DATA}==" | base64 -d)
+        local JSON=$(urldecode "${DATA_ENCODEURL}")
         #echo JSON: "${JSON}" #debug
-        v_errorNumber=$(get_json_value "${JSON}" errorNumber)
+        local v_errorNumber=$(get_json_value "${JSON}" errorNumber)
         #echo v_errorNumber=$v_errorNumber     #debug
         if [ "${v_errorNumber}" = "1" ]; then #认证成功
             CONNECT_TIME=$(date +%s)
             portalLink=$(get_json_value "${JSON}" portalLink)
-            v_jsonStr=${portalLink}
+            local v_jsonStr=${portalLink}
             logger -t "${BaseName}" -p user.info "Login Success"
             local TIME=$(date '+%Y-%m-%d %H:%M:%S')
             echo "Login Success "${TIME}""
@@ -154,7 +156,7 @@ function start_auth() {
                 start_auth
             fi
 
-            portalLink_ENCODEURL_DEBASE64="$(printf "%s" "${v_jsonStr}" | base64 -d)"
+            local portalLink_ENCODEURL_DEBASE64="$(printf "%s" "${v_jsonStr}" | base64 -d)"
             v_json=$(urldecode "${portalLink_ENCODEURL_DEBASE64}")
             if [ ! -n "${v_json}" ]; then #解码失败
                 logger -t "${BaseName}" -p user.err "portalLink DecodeURL error"
@@ -162,14 +164,14 @@ function start_auth() {
                 start_auth
             fi
             #echo v_json: "${v_json}" #debug
-            ifNeedModifyPwd=$(get_json_value $v_json ifNeedModifyPwd)
+            local ifNeedModifyPwd=$(get_json_value $v_json ifNeedModifyPwd)
             if [ "${ifNeedModifyPwd}" = true ]; then #要求修改密码
                 logger -t "${BaseName}" -p user.warn "Need Modify Pwd"
                 echo Need Modify Pwd
 
             fi
 
-            heartBeatCyc=$(get_json_value $v_json heartBeatCyc)
+            local heartBeatCyc=$(get_json_value $v_json heartBeatCyc)
             if [ $heartBeatCyc -gt 0 ]; then #要求心跳
                 requires_heartBeat=true
                 heartBeatCyc_TRUE=$(expr $heartBeatCyc / 1000)
@@ -185,12 +187,12 @@ function start_auth() {
 
         else #认证失败
             portServIncludeFailedCode=$(get_json_value "${JSON}" portServIncludeFailedCode)
-            portServIncludeFailedReason=$(get_json_value "${JSON}" portServIncludeFailedReason)
-            v_errorInfo=$(get_json_value "${JSON}" portServErrorCodeDesc)
+            local portServIncludeFailedReason=$(get_json_value "${JSON}" portServIncludeFailedReason)
+            local v_errorInfo=$(get_json_value "${JSON}" portServErrorCodeDesc)
             portServErrorCode=$(get_json_value "${JSON}" portServErrorCode)
             if [ -n "${portServIncludeFailedCode}" ]; then
-                portServFailedReason=$(get_json_value "${portServFailedReason_json}" "${portServIncludeFailedCode}")
-                logger -t "${BaseName}" -p user.err "Info: ${portServIncludeFailedReason}: ${portServFailedReason}"
+                local portServFailedReason=$(get_json_value "${portServFailedReason_json}" "${portServIncludeFailedCode}")
+                logger -t "${BaseName}" -p user.err "${portServIncludeFailedReason}: ${portServFailedReason}"
                 echo Info: "${portServIncludeFailedReason}": "${portServFailedReason}"
                 if [ "${portServIncludeFailedCode}" = "63013" -o "${portServIncludeFailedCode}" = "63015" -o "${portServIncludeFailedCode}" = "63018" -o "${portServIncludeFailedCode}" = "63025" -o "${portServIncludeFailedCode}" = "63026" -o "${portServIncludeFailedCode}" = "63031" -o "${portServIncludeFailedCode}" = "63032" -o "${portServIncludeFailedCode}" = "63100" ]; then
                     logger -t "${BaseName}" -p user.err "EXIT!"
